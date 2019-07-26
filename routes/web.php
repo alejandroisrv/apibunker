0f4c7ab50fa79176c461de28e7c28c4c8be48d5f
alejandro <alejandrojsrv@gmail.com>
2019-07-26 13:00:19 -0500

    subir

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,40 @@
 
 
 
-$router->post('/users/login',['uses'=> 'ClientController@getToken']);
+$router->post('/users/login',['uses'=> 'UsersController@getToken']);
 
 $router->group(['middleware' => ['auth']], function () use ($router) {
 
     
 
-    $router->get('/client/{id}',['uses'=> 'ClientController@getClient']);
-    $router->get('/clients',['uses'=> 'ClientController@getClients']);
     $router->post('/client',['uses'=> 'ClientController@create']);
+    $router->get('/clients',['uses'=> 'ClientController@getClients']);
+    $router->get('/client/{id}',['uses'=> 'ClientController@getClient']);
     $router->post('/client/update',['uses'=> 'ClientController@create']);
     $router->put('/client/{id}',['uses'=> 'ClientController@delete']);
+    
 
-
-    $router->get('/product/{id}',['uses'=> 'ProductController@getClient']);
-    $router->get('/products',['uses'=> 'ProductController@getClients']);
     $router->post('/product',['uses'=> 'ProductController@create']);
-    $router->post('/product/update',['uses'=> 'ProductController@create']);
+    $router->get('/product/{id}',['uses'=> 'ProductController@getProduct']);
+    $router->get('/products',['uses'=> 'ProductController@getProducts']);
+    $router->post('/product/update',['uses'=> 'ProductController@update']);
     $router->put('/product/{id}',['uses'=> 'ProductController@delete']);
 
 
-    $router->get('/order/{id}',['uses'=> 'ClientController@getClient']);
-    $router->get('/orders',['uses'=> 'ClientController@getClients']);
-    $router->post('/order',['uses'=> 'ClientController@create']);
-    $router->post('/order/update',['uses'=> 'ClientController@create']);
+    $router->post('/order',['uses'=> 'OrdersController@create']);
+    $router->get('/order/{id}',['uses'=> 'OrdersController@getOrder']);
+    $router->get('/orders',['uses'=> 'OrdersController@getOrders']);
+    $router->post('/order/delivered',['uses'=> 'OrdersController@delivered']);
     $router->put('/order/{id}',['uses'=> 'OrdersController@delete']);
 
 
-    $router->get('/pago/{id}',['uses'=> 'ClientController@getClient']);
-    $router->get('/pagos',['uses'=> 'ClientController@getClients']);
-    $router->post('/client',['uses'=> 'ClientController@create']);
-    $router->post('/client/update',['uses'=> 'ClientController@create']);
-    $router->put('/client/{id}',['uses'=> 'ClientController@delete']);
+    $router->get('/pay/{id}',['uses'=> 'PayController@getPay']);
+    $router->get('/pays',['uses'=> 'PayController@getPays']);
+    $router->post('/pay',['uses'=> 'PayController@create']);
+    $router->post('/pay/update',['uses'=> 'PayController@create']);
+    $router->put('/pay/{id}',['uses'=> 'PayController@delete']);
+
+    
 
 
 });
