46c487771cc49d14d6bef7c0d3bb6d2e0c022254
alejandro sojo <alejandrojsrv@gmail.com>
2020-01-10 22:24:09 -0500

    orderBy cat

--- app/Http/Controllers/V1/ProductosController.php
+++ app/Http/Controllers/V1/ProductosController.php
@@ -59,7 +59,10 @@ class ProductosController extends Controller
 
     function getCategorias(Request $request)
     {
-        $categorias = ProductoCategoria::select('id', 'nombre', 'imagen')->whereNotIn('id', [12, 13, 15])->get();
+        $categorias = ProductoCategoria::select('id', 'nombre', 'imagen')
+        ->whereNotIn('id', [12, 13, 15])
+        ->orderBy('nombre','ASC')
+        ->get();
 
         $categorias->map(function ($item) {
             $item->imagen = config('global.base_url') . 'assets/img/' . $item->imagen;
