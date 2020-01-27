f1c903021ec7788f82e33dc1ba6a04c326c083f2
alejandro sojo <alejandrojsrv@gmail.com>
2020-01-26 21:04:54 -0500

    pedidos

--- app/Http/Controllers/V1/PedidosController.php
+++ app/Http/Controllers/V1/PedidosController.php
@@ -75,8 +75,6 @@ class PedidosController extends Controller
             }
             
 
-            return response()->json(['response'],422);
-
             return response()->json(['response' => 'ok']);
 
         } catch (\Exception $e) {
